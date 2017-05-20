f1e10a63f1ef57292ee7660c817dfd262f5c93ab
Joseph Huynh <joe.huynh@gmail.com>
2017-05-20 01:06:17 -0700

    Fixed cart responsiveness. Moved cart.js into cart.php

--- html/cart.php
+++ html/cart.php
@@ -127,7 +127,7 @@ if (isset($_SESSION["cart"])) {
     ?>
 </p>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src="js/cart.js"></script>
+
 <script>
     $(document).ready(function () {
         // Load cart contents
@@ -158,6 +158,85 @@ if (isset($_SESSION["cart"])) {
         }
 
         console.log(cart);
+
+        /* Set rates + misc */
+        var taxRate = 0.08;
+        var shippingRate = 15.00;
+        var fadeTime = 300;
+
+        /* Assign actions */
+        $('#products').on('change', '.product-quantity input', function () {
+            updateQuantity(this);
+        });
+
+// $('.product-quantity input').change( function() {
+//   updateQuantity(this);
+// });
+
+        $('#products').on('click', '.product-removal button', function () {
+            removeItem(this);
+        });
+
+// $('.product-removal button').click( function() {
+//   removeItem(this);
+// });
+
+        /* Recalculate cart */
+        function recalculateCart() {
+            var subtotal = 0;
+
+            /* Sum up row totals */
+            $('.product').each(function () {
+                subtotal += parseFloat($(this).children('.product-line-price').text());
+            });
+
+            /* Calculate totals */
+            var tax = subtotal * taxRate;
+            var shipping = (subtotal > 0 ? shippingRate : 0);
+            var total = subtotal + tax + shipping;
+
+            /* Update totals display */
+            $('.totals-value').fadeOut(fadeTime, function () {
+                $('#cart-subtotal').html(subtotal.toFixed(2));
+                $('#cart-tax').html(tax.toFixed(2));
+                $('#cart-shipping').html(shipping.toFixed(2));
+                $('#cart-total').html(total.toFixed(2));
+                if (total == 0) {
+                    $('.checkout').fadeOut(fadeTime);
+                } else {
+                    $('.checkout').fadeIn(fadeTime);
+                }
+                $('.totals-value').fadeIn(fadeTime);
+            });
+        }
+
+        /* Update quantity */
+        function updateQuantity(quantityInput) {
+            /* Calculate line price */
+            var productRow = $(quantityInput).parent().parent();
+            var price = parseFloat(productRow.children('.product-price').text());
+            var quantity = $(quantityInput).val();
+            var linePrice = price * quantity;
+
+            /* Update line price display and recalc cart totals */
+            productRow.children('.product-line-price').each(function () {
+                $(this).fadeOut(fadeTime, function () {
+                    $(this).text(linePrice.toFixed(2));
+                    recalculateCart();
+                    $(this).fadeIn(fadeTime);
+                });
+            });
+        }
+
+        /* Remove item from cart */
+        function removeItem(removeButton) {
+            /* Remove row from DOM and recalc cart total */
+            var productRow = $(removeButton).parent().parent();
+            productRow.slideUp(fadeTime, function () {
+                productRow.remove();
+                recalculateCart();
+            });
+        }
     });
 </script>
 
